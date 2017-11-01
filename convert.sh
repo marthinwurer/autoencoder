aa327e6d76dd43318f84d6037ba156b4911c5f37
Benjamin Maitland <marthinwurer@gmail.com>
2017-10-31 22:47:59 -0400

    Initial commit. Convert script works!

---
+++ convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# this file will be used to convert images to the correct format to be added to a dataset
+IN_DIR=$1
+OUT_DIR=$2
+
+CUR_NUM=`ls $OUT_DIR | sed -e 's/[^1-9]*\([0-9]*\)[^0-9]*/\1/' | sort -n | tail -1`
+echo $CUR_NUM
+CUR_NUM=$[$CUR_NUM + 1]
+echo $CUR_NUM
+
+for f in $(ls $IN_DIR )
+do
+    echo $f $CUR_NUM
+    convert -resize 128x64 $IN_DIR$f $OUT_DIR/out-$(printf "%05d" $CUR_NUM).png && CUR_NUM=$[$CUR_NUM + 1]
+done
---
+++ main.py
@@ -0,0 +1,26 @@
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.models import Model
+from keras import backend as K
+
+input_img = Input(shape=(128, 64, 3))  # adapt this if using `channels_first` image data format
+
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+encoded = MaxPooling2D((2, 2), padding='same')(x)
+
+# at this point the representation is (4, 4, 8) i.e. 128-dimensional
+
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
+
+autoencoder = Model(input_img, decoded)
+autoencoder.summary()
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
