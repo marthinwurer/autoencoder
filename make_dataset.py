731c8d6e9eee7132b6dc5f1d29a0830a4e1f094c
Benjamin Maitland <marthinwurer@gmail.com>
2018-01-29 00:45:25 -0500

    Got saving and basic loading working

--- make_dataset.py
+++ make_dataset.py
@@ -15,6 +15,36 @@ import os
 # print(type(x_train), x_train.shape, x_train.dtype)
 # print(type(y_train), y_train.shape, y_train.dtype)
 
+TRAIN_DATA_FILE = "train_data.npy"
+TRAIN_LABEL_FILE = "train_labels.npy"
+TEST_DATA_FILE = "test_data.npy"
+TEST_LABEL_FILE = "test_labels.npy"
+NAMES_FILE = "names.csv"
+
+def load_dataset(path):
+    # load the arrays
+    train_data = numpy.load(path+"/"+TRAIN_DATA_FILE)
+    train_labels = numpy.load(path+"/"+TRAIN_LABEL_FILE)
+    test_data = numpy.load(path+"/"+TEST_DATA_FILE)
+    test_labels = numpy.load(path+"/"+TEST_LABEL_FILE)
+
+    # load the names file
+    names = {}
+    with open(NAMES_FILE) as f:
+        for line in f:
+            line = line.split(",")
+            names[line[0].strip()] = int(line[1].strip())
+
+    print(train_data.shape)
+    print(test_data.shape)
+    print(names)
+
+    # expand the labels into one-hot vectors
+
+
+    return (train_data, train_labels), (test_data, test_labels)
+
+
 def load_images(name: str, directories: set, names=None):
     images = []
     labels = []
@@ -36,9 +66,9 @@ def load_images(name: str, directories: set, names=None):
 
         # show last image
         print(type(image), image.shape, image.dtype)
-        plt.imshow(image)
-        plt.show()
-        print(image)
+        # plt.imshow(image)
+        # plt.show()
+        # print(image)
 
     images = numpy.asarray(images)
     labels = numpy.asarray(labels)
@@ -79,6 +109,34 @@ def main( argv):
 
     print("Directories match, generating train files")
     images, labels, names = load_images(train_dir, train_dirs)
+
+    print(type(images), images.shape, images.dtype)
+
+    # save images
+    numpy.save("train_data.npy", images)
+    numpy.save("train_labels.npy", labels)
+
+    # then do the test files
+    images, labels, names = load_images(test_dir, train_dirs)
+    print(type(images), images.shape, images.dtype)
+
+    numpy.save("test_data.npy", images)
+    numpy.save("test_labels.npy", labels)
+
+    # dump the names file
+    with open(NAMES_FILE, "w") as f:
+        for k, v in names.items():
+            f.write(k + ", " + str(v) + "\n")
+
+    plt.imshow(images[0])
+    plt.show()
+
+
+    (train_images, train_labels), (test_images, test_labels) = load_dataset(".")
+
+    plt.imshow(test_images[0])
+    plt.show()
+
 
 
 
