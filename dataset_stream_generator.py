85a7f3a424d58a0424d437d36a1a1888c9c96fc3
Benjamin Maitland <marthinwurer@gmail.com>
2018-02-01 12:10:58 -0500

    added a script to process the coil-20 dataset

---
+++ dataset_stream_generator.py
@@ -0,0 +1,14 @@
+"""
+Based on https://gist.github.com/jfsantos/e2ef822c744357a4ed16ec0c885100a3#gistcomment-2325616
+"""
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, AveragePooling2D, Dropout
 from keras.models import Model
 from keras.optimizers import Adam, SGD
 from keras.datasets import cifar10
@@ -8,19 +8,24 @@ import make_dataset
 input_img = Input(shape=(128, 256, 3))  # adapt this if using `channels_first` image data format
 
 x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
-x = MaxPooling2D((2, 2), padding='same')(x)
+x = AveragePooling2D((2, 2), padding='same')(x)
+x = Dropout(0.1)(x)
 x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = MaxPooling2D((2, 2), padding='same')(x)
+x = AveragePooling2D((2, 2), padding='same')(x)
+x = Dropout(0.1)(x)
 x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
 encoded = MaxPooling2D((2, 2), padding='same')(x)
 
 # at this point the representation is (16, 8, 8) i.e. 128-dimensional
 
 x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = Dropout(0.1)(x)
 x = UpSampling2D((2, 2))(x)
 x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = Dropout(0.1)(x)
 x = UpSampling2D((2, 2))(x)
 x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+x = Dropout(0.1)(x)
 x = UpSampling2D((2, 2))(x)
 decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
 
@@ -32,7 +37,7 @@ opt = Adam()
 
 
 
-autoencoder.compile(optimizer=opt, loss='mean_squared_error')
+autoencoder.compile(optimizer=opt, loss='binary_crossentropy')
 
 
 # set up the data
